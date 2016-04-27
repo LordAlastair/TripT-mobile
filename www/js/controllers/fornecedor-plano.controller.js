7d315dff01b6446ce8e10b8ecdd636d8406dc90d
Shayron <shayron.aguiar@gmail.com>
2016-04-27 16:33:36 -0300

    Plano Service/Iniciando Controller/Melhoria template

--- www/js/controllers/fornecedor-plano.controller.js
+++ www/js/controllers/fornecedor-plano.controller.js
@@ -1,6 +1,32 @@
 angular
 .module('app.controllers')
-.controller('FornecedorPlanoCtrl', function($scope, FornecedorPlanoService, $ionicPopup, $ionicLoading) {
+.controller('FornecedorPlanoCtrl', function($scope, FornecedorPlanoService, PlanoService, $ionicPopup, $ionicLoading) {
+
+  $scope.fornecedorplano = {};
+  $scope.planos = {};
+
+  function _getPlanos(){
+    PlanoService
+    .query()
+    .then(function(planos){
+      $scope.planos = planos;
+    });
+  };
+
+  function _getFornecedorPlano(){
+    FornecedorPlanoService
+    .load()
+    .then(function (res){
+      $scope.fornecedorplano = res.data;
+      console.log(res.data);
+    });
+  };
+
+  var $scope.checkedPlanos = fornecedorplano.map(fornecedoplano => fornecedorplano.fop_cd_plano);
+//var $scope.checkedPlanos = fornecedorplano.map(function(fornecedoplano){ return fornecedorPlano.fop_cd_plano;};);
+//fornecedorPlanos.forEach(function(fornecedorPlano) {
+//  checkedPlanos.push(fornecedorPlano.fop_cd_plano);
+//});
 
 
 
