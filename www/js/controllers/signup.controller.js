b625b5c2799e3859239fe4983cc5fc0f8c511240
Shayron Aguiar <shayron.aguair@gmail.com>
2016-05-16 01:04:41 -0300

    validate password on singup

--- www/js/controllers/signup.controller.js
+++ www/js/controllers/signup.controller.js
@@ -2,8 +2,17 @@ angular
 .module('app.controllers')
 .controller('SignupCtrl', function($scope, Usuario, $state, $ionicLoading, $ionicPopup) {
   $scope.usuario = {};
+  $scope.required = true;
+
 
   $scope.signup = function() {
+    if(!_comparePass()){
+      return;
+    }
+    if(!_validPass()){
+      return;
+    }
+
     $ionicLoading.show();
 
     Usuario
@@ -33,4 +42,14 @@ angular
   function _finally() {
     $ionicLoading.hide();
   }
+
+  function _comparePass() {
+    return angular.equals($scope.usuario.usu_ds_senha, $scope.usuario.confirm_usu_ds_senha)
+  };
+
+  function _validPass(){
+    return $scope.usuario.usu_ds_senha.length > 5;
+  }
+
+
 })
